68faf0750f4c0af140592108e4c3f2958fda839a
VarvaraKirilenko <95625082+VarvaraKirilenko@users.noreply.github.com>
2022-03-03 13:31:52 +0300

    Update Test.java

--- pr1/Test.java
+++ pr1/Test.java
@@ -1,1 +1,12 @@
+package ru.mirea2.pr1;
 
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args){
+        Scanner console = new Scanner(System.in);
+        int n = console.nextInt();
+        Task t = new Task(n);
+        System.out.println(t.test(n));
+    }
+}
